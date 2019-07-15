1eee5252035abe5a5cccd994ac4d61f1b3cf03d2
GYengera <g.yengera@gmail.com>
2019-07-15 19:17:52 +0200

    paired checking error fix

--- data/pix2pix_dataset.py
+++ data/pix2pix_dataset.py
@@ -65,9 +65,10 @@ class Pix2pixDataset(BaseDataset):
 
         # input image (real images)
         image_path = self.image_paths[index]
-        assert self.paths_match(label_path, image_path), \
-            "The label_path %s and image_path %s don't match." % \
-            (label_path, image_path)
+        if not opt.no_pairing_check:
+	        assert self.paths_match(label_path, image_path), \
+    	        "The label_path %s and image_path %s don't match." % \
+        	    (label_path, image_path)
         image = Image.open(image_path)
         image = image.convert('RGB')
 
